291f6d09c069fca41962be0b6bd3605a81e606b3
Denis Malinin <melowetty@mail.ru>
2023-01-06 03:46:44 +0500

    Отрисован header главного экрана

--- app/src/main/java/com/sadteam/assistantformafia/MainActivity.kt
+++ app/src/main/java/com/sadteam/assistantformafia/MainActivity.kt
@@ -3,13 +3,24 @@ package com.sadteam.assistantformafia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sadteam.assistantformafia.ui.theme.AssistantForMafiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,24 +31,79 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("HSE Perm")
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Header()
+                    }
                 }
             }
         }
     }
 }
 
+/**
+ * Круглая кнопка с иконкой внутри
+ * TODO необходимо сделать действие на кнопку
+ *
+ * @param painter иконка
+ * @param backgroundColor цвет фона-круга сзади иконки
+ * @param description описание действия кнопки
+ */
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello, $name!")
+fun IconButton(
+    painter: Painter,
+    backgroundColor: Color,
+    description: String,
+) {
+    Box(
+        modifier = Modifier
+            .width(40.dp)
+            .height(40.dp)
+            .background(
+                color = backgroundColor,
+                shape = CircleShape
+            ),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = description,
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
 
-@Preview(showBackground = true)
+/**
+ * Шапка с как начать играть и настройками
+ *
+ */
 @Composable
-fun DefaultPreview() {
-    AssistantForMafiaTheme {
-        Greeting("HSE Perm")
+fun Header() {
+    val backgroundColor = Color(0xFF132C3A)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(
+            painter = painterResource(id = R.drawable.ic_baseline_question_mark_24),
+            backgroundColor = backgroundColor,
+            description = stringResource(id = R.string.how_to_play_button)
+        )
+        Text(
+            text = stringResource(id = R.string.game_settings),
+            fontSize = 24.sp
+        )
+        IconButton(
+            painter = painterResource(id = R.drawable.ic_baseline_settings_24),
+            backgroundColor = backgroundColor,
+            description = stringResource(id = R.string.settings_button)
+        )
     }
 }
