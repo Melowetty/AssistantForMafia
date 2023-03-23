7aa8a0d0dc48b3e730488bb74cb4675a1e177b76
Denis Malinin <melowetty@mail.ru>
2023-03-24 02:30:02 +0500

    fix game creation tests & add unit tests for converters

--- app/src/main/java/com/sadteam/assistantformafia/utils/Converters.kt
+++ app/src/main/java/com/sadteam/assistantformafia/utils/Converters.kt
@@ -4,15 +4,18 @@ import androidx.room.TypeConverter
 import com.sadteam.assistantformafia.data.models.Possibility
 
 class Converters {
-        @TypeConverter
-        fun fromPossibilities(possibilities: List<Possibility>): String {
-            return possibilities.joinToString(separator = ",")
-        }
+    @TypeConverter
+    fun fromPossibilities(possibilities: List<Possibility>): String {
+        return possibilities.joinToString(separator = ",")
+    }
 
-        @TypeConverter
-        fun toPossibilities(possibilities: String): List<Possibility> {
-            return possibilities.split(",").map {
-                Possibility.valueOf(it)
-            }
+    @TypeConverter
+    fun toPossibilities(possibilities: String): List<Possibility> {
+        if (possibilities.isEmpty()) {
+            return listOf()
         }
+        return possibilities.split(",").map {
+            Possibility.valueOf(it)
+        }
+    }
 }
